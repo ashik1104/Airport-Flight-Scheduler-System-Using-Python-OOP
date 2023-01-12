48928d9c001aee5d077326eaeed78863197ad291
Ashik Mahmud <118339165+ashik1104@users.noreply.github.com>
2023-01-12 11:43:30 +0600

    Delete Read_friend_age.py

--- Read_friend_age.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('./Data/My_Friends.csv', 'r') as file:
-    lines = csv.reader(file)
-    header = next(lines)
-    for line in lines:
-        print(line)
-    print(header)
